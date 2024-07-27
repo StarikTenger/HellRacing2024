a3521ff69e70b48a7e65b1aaf55319e4ada46abd
StarikTenger <StarikTenger@gmail.com>
2024-07-27 15:34:12 +0200

    holy water edition 1

---
+++ scenes/cooldown_bonus.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var player : CharacterBody2D = $"../Skull"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body == player:
+		print("enter")
+	
