d263fa44013f32e964a09ff7ea723d0f03b679bc
Yury Solopov <solopov.yuriy2@wb.ru>
2024-07-27 13:48:18 +0300

    changed input actions

--- scripts/Skull.gd
+++ scripts/Skull.gd
@@ -1,30 +1,31 @@
 extends CharacterBody2D
 
-@export var speed : float = 100
+@export var force : float = 100
 @export var basic_speed : float = 200
 @export var turn_speed : float = 5
+@export var speed_threshold : float = 1000
 @export var friction_k : float = 2.5
 @export var turn_smoothness : float = 0.1
 @export var target_rotation : float = 0.0
 
+
 var acceleration_on = true
 
 func change_speed(delta):
-	speed *= pow(1.2, delta)
-	var threshold : float = 1000
-	if speed > threshold:
-		speed = threshold
+	force *= pow(1.2, delta)
+	if force > speed_threshold:
+		force = speed_threshold
 
 func slow_down():
 	acceleration_on = false
-	speed = basic_speed
+	force = basic_speed
 	$Cooldown.start()
 
 func _physics_process(delta):
 	# Acceleration
 	var direction : Vector2 = Vector2(1, 0).rotated(rotation)
 	if acceleration_on :
-		velocity += direction * speed * delta
+		velocity += direction * force * delta
 		change_speed(delta)
 
 	# Friction
@@ -33,27 +34,23 @@ func _physics_process(delta):
 	velocity -= velocity * direction_k * friction_k * delta;
 
 	# Rotation control
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("left"):
 		target_rotation -= turn_speed * delta
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("right"):
 		target_rotation += turn_speed * delta
 	
 	rotation = lerp_angle(rotation, target_rotation, turn_smoothness)
 	
 	# Test slowdown
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("down"):
 		slow_down()
 	
 	move_and_slide()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("SkullAnimation").play()
-	speed = basic_speed
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$SkullAnimation.play()
+	force = basic_speed
 
 
 func _on_cooldown_timeout():
