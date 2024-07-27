5948aedeb0c1f847130ff21781bb82ff31f7e407
StarikTenger <StarikTenger@gmail.com>
2024-07-27 16:00:00 +0200

    smth

---
+++ scripts/OverheatBar.gd
@@ -0,0 +1,13 @@
+extends TextureProgressBar
+
+@export var skull: Skull
+
+func _ready():
+	skull = $"../../../Skull"
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	update()
+
+func update():
+	value = skull.heat * 100
