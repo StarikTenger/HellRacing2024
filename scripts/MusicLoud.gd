bfb4e42ef3b5ef4ddb6409ebae1cc3668e69da12
StarikTenger <StarikTenger@gmail.com>
2024-07-28 19:43:36 +0200

    new music

--- scripts/MusicLoud.gd
+++ scripts/MusicLoud.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	volume_db = ($"../Skull".heat - 1) * 80
+	volume_db = ($"../Skull".heat - 1) * 30 - 7
